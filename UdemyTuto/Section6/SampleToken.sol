34a427d9f031b7f0d725336885224b738f1d65b4
shinhzy <shin.hzy@gmail.com>
2023-07-05 18:09:34 +0900

    Change codes and Add comments

--- UdemyTuto/Section6/SampleToken.sol
+++ UdemyTuto/Section6/SampleToken.sol
@@ -1,6 +1,28 @@
 //[6.ERC20 Token Sale]
 //<The ERC20 Token Explained>
 //  {SampleToken.sol} <<<
+//<Implementing An ERC20 Token Sale>
+//  {SampleTokenSale.sol}
+
+
+//<1. {SampleToken.sol} 홀로 테스트 >
+//
+
+//<2. {SampleToken.sol}, {SampleTokenSale.sol} 함께 테스트 >
+// 가상 어카운트 A,B,C 가 있을 때,
+// A 어카운트가 디플로이한 X(A){ SampleToken.sol > AccessControl } 컨트랙트가 있다.
+// X(A) 컨트랙트에서 A 어카운트로 < to: B 주소, amount: 100 >로 mint 하였다.
+// X(A) 컨트랙트에서 "         " < account: B 주소 >로 balanceOf로 토큰 잔액 확인하였다.
+// B 어카운트로 Y(B) { SampleTokenSale.sol > TokenSale } 컨트랙트 디플로이하고자 할 때
+//                                          컨트랙트 X(A) 주소를 넣으며 디플로이하였다.
+// X(A) 컨트랙트에서 B 어카운트로 < spender: Y(B) 주소, addedValue: 5*10"^18 > 값을 increaseAllowance에 입력하여 트랜잭션하였다.
+// "              " "         " < ownder: B 의 주소, spender: Y(B) 주소> 를 allowance 값에 넣고 출력을 확인하였다.
+// C 어카운트로 < value: 1 ETH > 와 함께 Y(B) 컨트랙트에서 purchase 시도하였으며, 성공하였다.
+// 이때 X(A) 컨트랙트에서 C 어카운트로 < ownder: B 의 주소, spender: Y(B) 주소> 를 allowance 값에 넣고 출력을 확인하면
+// 값이 1 ETH 감소하여 4*10^18 이 된 것을 확인할 수 있다.
+
+
+
 
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
@@ -23,19 +45,20 @@ contract CoffeeToken is ERC20, AccessControl {
         _grantRole(MINTER_ROLE, msg.sender);
     }
 
-    //mintable 상태 == 스마트 컨트렉트 배포 후에 민팅 가능
+    //mintable 상태 == 스마트 컨트랙트 배포 후에 민팅 가능
     //특정 지갑에 토큰 제공
     //현 예제에서 10개 제공 예정
     function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
-        _mint(to, amount);
+        // _mint(to, amount); // 최소단위인 Wei 단위 기준
+        _mint(to, amount * 10 ** decimals()); // Ether 단위 기준 _ 10의 18제곱
     }
 
     //_msgSender() 는 msg.sender와 거의 같으나,
-    //중간 컨트렉트에서 사용자의 요청을 받아서 대신 가스비를 소모하고 요청해주는 Gas Station Network구조에서,
-    //  relayer(사용자의 요청을 받아 다시보낸 컨트렉트)가 아닌 end-user(요청 보낸 사용자)를
+    //중간 컨트랙트에서 사용자의 요청을 받아서 대신 가스비를 소모하고 요청해주는 Gas Station Network구조에서,
+    //  relayer(사용자의 요청을 받아 다시보낸 컨트랙트)가 아닌 end-user(요청 보낸 사용자)를
     //  MessageSender로 명확히 지정하기 위해 사용함.
     //
-    //이를 통해 중간 다리인 컨트렉트가 msg.sender가 되는 것을 방지함
+    //이를 통해 중간 다리인 컨트랙트가 msg.sender가 되는 것을 방지함
     //
     // User A -> Contract A -> Contract B 일 때, Contract B입장에서
     // msg.sender는 Contract A 이고 /  _msgSender()는 User A 임.
@@ -46,17 +69,21 @@ contract CoffeeToken is ERC20, AccessControl {
     //<특정 지갑이 직접 커피를 구매하는 경우>
     //자신이 수령자이자 구매자
     function buyOneCoffee() public {
-        _burn(_msgSender(), 1);
+        // _burn(_msgSender(), 1); // 최소단위인 Wei 단위 기준
+        _burn(_msgSender(), 1 * 10 ** decimals()); // Ether 단위 기준 _ 10의 18제곱
         emit CoffeePurchased(_msgSender(),_msgSender());
-    }
+    } 
+    
     //
     //<특정 지갑이 다른 이의 지갑을 통해 커피를 구매하는 경우>
     //allowance를 통해 다른 지갑이 자신의 지갑에서 특정 개수의 토큰을 사용할 수 있게 허가해두면
     //나중에 그 다른 지갑이 나중에 자신의 지갑에서 토큰을 소모하며 커피를 사게 됨
     //구매 요청자가 수령자, allowance배포한 지갑이 실제 구매자
     function buyOneCoffeeFrom (address account) public {
-        _spendAllowance(account, _msgSender(), 1);
-        _burn(account, 1);
+        // _spendAllowance(account, _msgSender(), 1);
+        // _burn(account, 1); // 최소단위인 Wei 단위 기준
+        _spendAllowance(account, _msgSender(), 1 * 10 ** decimals());
+        _burn(account, 1 * 10 ** decimals()); // Ether 단위 기준 _ 10의 18제곱
         emit CoffeePurchased(_msgSender(), account);
     }
 
--- UdemyTuto/Section6/SampleTokenSale.sol
+++ UdemyTuto/Section6/SampleTokenSale.sol
@@ -1,17 +1,73 @@
+//[6.ERC20 Token Sale]
+//<The ERC20 Token Explained>
+//  {SampleToken.sol}
+//<Implementing An ERC20 Token Sale>
+//  {SampleTokenSale.sol} <<<
+
+
+//<1. {SampleToken.sol}, {SampleTokenSale.sol} 함께 테스트 >
+// 가상 어카운트 A,B,C 가 있을 때,
+// A 어카운트가 디플로이한 X(A){ SampleToken.sol > AccessControl } 컨트랙트가 있다.
+// X(A) 컨트랙트에서 A 어카운트로 < to: B 주소, amount: 100 >로 mint 하였다.
+// X(A) 컨트랙트에서 "         " < account: B 주소 >로 balanceOf로 토큰 잔액 확인하였다.
+// B 어카운트로 Y(B) { SampleTokenSale.sol > TokenSale } 컨트랙트 디플로이하고자 할 때
+//                                          컨트랙트 X(A) 주소를 넣으며 디플로이하였다.
+// X(A) 컨트랙트에서 B 어카운트로 < spender: Y(B) 주소, addedValue: 5*10"^18 > 값을 increaseAllowance에 입력하여 트랜잭션하였다.
+// "              " "         " < ownder: B 의 주소, spender: Y(B) 주소> 를 allowance 값에 넣고 출력을 확인하였다.
+// C 어카운트로 < value: 1 ETH > 와 함께 Y(B) 컨트랙트에서 purchase 시도하였으며, 성공하였다.
+// 이때 X(A) 컨트랙트에서 C 어카운트로 < ownder: B 의 주소, spender: Y(B) 주소> 를 allowance 값에 넣고 출력을 확인하면
+// 값이 1 ETH 감소하여 4*10^18 이 된 것을 확인할 수 있다.
+
+
+
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.16;
 
+// // 엄격한 추상 컨트랙트 __ 틀의 역할만 함
+// //    모든 함수가 추상함수, 다른 컨트랙트나 인터페이스 피상속 불가
+// //    자체 생성자, 변수, 구조체 및 나열형 정의 불가
+// interface IERC20 {
+//     function transfer(address to, uint amount) external;
+//     //토큰 이체 기능
+//     function decimals() external view returns(uint);
+//     //
+// }
+
+// contract TokenSale {
+//     uint tokenPriceInWei = 1 ether;
+//     //변수에 1 Ether == 10^18 Wei 저장
+
+//     IERC20 token; //추상 컨트랙트 인터페이스 참조
+
+//     constructor(address _token) {
+//         token = IERC20(_token);
+//     } // 컨트랙트 배포될 때, 토큰 주소 입력 받음.
+
+//     function purchase() public payable {
+//         require(msg.value >= tokenPriceInWei, "Not enough money sent"); // 결제량 확인
+//         uint tokensToTransfer = msg.value / tokenPriceInWei; //구매 가능한 토큰 수 계산
+//         uint remainder = msg.value - tokensToTransfer * tokenPriceInWei; // 전송되지 않은 나머지 이더를 계산                                                                                       
+//         token.transfer(msg.sender, tokensToTransfer * 10 ** token.decimals()); // 구매된 토큰 구매자에게 전송 __ Ether 단위
+//         payable(msg.sender).transfer(remainder); //send the rest back // 구매후 남은 이더 구매자에게 반환
+
+//     }
+// }
+
+//==Not refined version
+// 추상 컨트랙트 __ 틀의 역할 + 자체 기능 존재
 abstract contract ERC20{
     function transferFrom (address _from, address _to, uint256 _value) public virtual returns (bool success);
     function decimals() public virtual view returns (uint8);
 }
 
 contract TokenSale {
-    uint tokenPriceInWei = 1 ether;
+    uint public tokenPriceInWei = 1 ether;
 
     ERC20 public token;
+    address public tokenOwner;
 
     constructor(address _token) {
+        tokenOwner = msg.sender;
         token = ERC20(_token);
     }
 
@@ -19,7 +75,7 @@ contract TokenSale {
         require(msg.value >= tokenPriceInWei, "Not enough money sent");
         uint tokensToTransfer = msg.value / tokenPriceInWei;
         uint remainder = msg.value - tokensToTransfer * tokenPriceInWei;
-        token.transfer(msg.sender, tokensToTransfer * 10 ** token.decimals());
+        token.transferFrom(tokenOwner, msg.sender, tokensToTransfer * 10 ** token.decimals()); // Wei 단위
         payable(msg.sender).transfer(remainder); //send the rest back
 
     }
